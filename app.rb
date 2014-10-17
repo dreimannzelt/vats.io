bba3f801c516db437a48ef077f4fbca946ea8295
Björn Wolf <bjoern@dreimannzelt.de>
2014-10-17 20:44:45 +0200

    Prett JSON response and valid configs

--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 require 'sinatra'
 
 get '/names.json' do
-  content_type :json
-  File.read(settings.root + "/names.json")
+  content_type 'application/json', charset: 'utf-8'
+  JSON.parse( File.read(settings.root + "/names.json") ).to_json
+end
+
+get '/names/:locale.json' do
+  content_type 'application/json', charset: 'utf-8'
+  JSON.parse( File.read(settings.root + "/names.json") )[params[:locale]].to_json
 end
 
 get '/rates.json' do
-  content_type :json
-  File.read(settings.root + "/rates.json")
+  content_type 'application/json', charset: 'utf-8'
+  JSON.parse( File.read(settings.root + "/rates.json") ).to_json
+end
+
+get '/rates/:locale.json' do
+  content_type 'application/json', charset: 'utf-8'
+  JSON.parse( File.read(settings.root + "/rates.json") )[params[:locale]].to_json
 end
